a00937cbf20c27ffff5982764a665f4abf8c6d26
Evan Clough & Brian Lau <student@epicodus.com>
2015-08-11 11:37:12 -0700

    Create methods #name, #food_level, #sleep_level, #activity_level to return the respective instance variables of the same names.

--- lib/tamagochi.rb
+++ lib/tamagochi.rb
@@ -7,7 +7,19 @@ class Tamagochi
     @activity_level = activity_level
   end
 
-  define
+  define_method(:name) do
+    @name
+  end
 
+  define_method(:food_level) do
+    @food_level
+  end
 
+  define_method(:sleep_level) do
+    @sleep_level
+  end
+
+  define_method(:activity_level) do
+    @activity_level
+  end
 end
--- spec/tamagochi_spec.rb
+++ spec/tamagochi_spec.rb
@@ -1,10 +1,10 @@
 require('rspec')
 require('tamagochi')
 
-describe(Tamagotchi) do
+describe(Tamagochi) do
   describe("#initialize") do
     it("sets the name and life levels of a new Tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagochi.new("lil dragon")
       expect(my_pet.name()).to(eq("lil dragon"))
       expect(my_pet.food_level()).to(eq(10))
       expect(my_pet.sleep_level()).to(eq(10))
@@ -14,12 +14,12 @@ describe(Tamagotchi) do
 
   describe("#is_alive") do
     it("is alive if the food level is above 0") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagochi.new("lil dragon")
       expect(my_pet.is_alive()).to(eq(true))
     end
 
     it("is dead if the food level is 0") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagochi.new("lil dragon")
       my_pet.set_food_level() #make a method that will change the food level of your tamagotchi.
       expect(my_pet.is_alive()).to(eq(false))
     end
@@ -27,7 +27,7 @@ describe(Tamagotchi) do
 
   describe("#time_passes") do
     it("decreases the amount of food the Tamagotchi has left by 1") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagochi.new("lil dragon")
       my_pet.time_passes()  #deciding how to make time pass can be tricky
       expect(my_pet.food_level()).to(eq(9))
     end
