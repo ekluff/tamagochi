80256fa3fa5f088ab37274f660257cbb8fa3db3e
Evan Clough & Brian Lau <student@epicodus.com>
2015-08-11 11:30:46 -0700

    Write initialize method for class Tamagochi. It will create new Tamagochi instances with name, food_level, sleep_level, and activity_level.

--- lib/tamagochi.rb
+++ lib/tamagochi.rb
@@ -1,4 +1,13 @@
 class Tamagochi
-  
+
+  define_method(:initialize) do |name = "Tacocat", food_level = 10, sleep_level = 10, activity_level = 10|
+    @name = name
+    @food_level = food_level
+    @sleep_level = sleep_level
+    @activity_level = activity_level
+  end
+
+  define
+
 
 end
